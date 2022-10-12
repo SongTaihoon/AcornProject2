d58dd101433b7942b85c9a74e315ab046d8afa1e
임준성 <mng6975@naver.com>
2022-10-12 20:23:40 +0900

    pull 수정 보완

--- mainPage/src/main/java/com/project/mainPage/dto/Reply.java
+++ mainPage/src/main/java/com/project/mainPage/dto/Reply.java
@@ -28,6 +28,7 @@ public class Reply {
 	private String img_path;  // 댓글 이미지 
 	private int board_no;
 	private UserDto user;  // UsersDto userid : fk
+	private int remove_img_check; // 댓글에서 이미지 체크하여 삭제할 수 있도록 하기 위해 추가
 	private int good;
 	private int bad;
 	private Boolean prefer_active = null; // null : 누른 적이 없는, true : good를 누른 것, false: bad를 누른 것 
