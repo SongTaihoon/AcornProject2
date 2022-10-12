f62a171177681bc377e8357cdd67368c9c94a9cb
임준성 <mng6975@naver.com>
2022-10-12 15:15:12 +0900

    관광지 식당 전화번호 추가

--- mainPage/src/main/resources/static/public/js/top/tour/list.js
+++ mainPage/src/main/resources/static/public/js/top/tour/list.js
@@ -36,9 +36,10 @@
 	        var  names = this.cells[1].innerText; 
 	        var  region = this.cells[2].innerText;
 	        var  cate = this.cells[3].innerText;
-			var  address = this.cells[4].innerText;
-	        var  search = this.cells[5].innerText;
-	         
+			var  phone = this.cells[4].innerText;
+			var  address = this.cells[5].innerText;
+	        var  search = this.cells[6].innerText;
+	        var  number = this.cells[7].innerText;
 	
 	// 주소-좌표 변환 객체를 생성합니다
 	var geocoder = new kakao.maps.services.Geocoder();
@@ -61,9 +62,10 @@
 	        kakao.maps.event.addListener(marker, 'click', function() {        
    	    
 		        // 인포윈도우로 장소에 대한 설명을 표시합니다
-		        var iwContent='<div style="width:200px;margin:5px;padding:6px 0;"><div style="font-size:22px;margin:10px 10px 1px;"><strong>'+names+
+		        var iwContent='<div style="width:250px;margin:4px;padding:6px 0;"><div style="font-size:22px;margin:10px 10px 1px;"><strong>'+names+
 		        '</strong></div><br><div style="font-size:15px;margin:5px;">'+address+
-		        '</div><a style="display:flex;justify-content:center;" class="btn" href="https://map.kakao.com/link/to/'+names+','+
+		        '</div><div style="font-size:14px;margin:1px 10px 1px;"><span>'+search+'</span><span style="margin:1px 10px 1px;"><a style="text-decoration:none;" href="/top/tour/detail/'+number+
+		        '">상세보기</a></span></div><a style="display:flex;justify-content:center;" class="btn" href="https://map.kakao.com/link/to/'+names+','+
 		        marker.getPosition().getLat()+','+marker.getPosition().getLng()+'" target="_blank">길찾기</a></div>';
 		        var	iwRemoveable = true;
 		        
@@ -75,10 +77,12 @@
 		        infowindow.open(map, marker);  
 			});
 
+			kakao.maps.event.addListener(marker, 'rightclick', function() {
+				marker.setMap(null);
+			});
 			
 	        // 지도의 중심을 결과값으로 받은 위치로 이동시킵니다
 	        map.setCenter(coords);
-			
 	    } 
 	    
 	});  
