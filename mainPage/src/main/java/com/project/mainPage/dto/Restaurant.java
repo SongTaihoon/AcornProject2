0c536ecf4a9201232ca7f9f96e6b8507925ddaca
Kevin <kvpark98@naver.com>
2022-10-20 09:23:45 +0900

    나의 여행 추천 모달 창, 추천 결과 페이지 구현

--- mainPage/src/main/java/com/project/mainPage/dto/Restaurant.java
+++ mainPage/src/main/java/com/project/mainPage/dto/Restaurant.java
@@ -1,7 +1,7 @@
 package com.project.mainPage.dto;
 import java.util.List;
 import lombok.Data;
-/*
+/*desc Restaurant;
 +-------------+--------------+------+-----+---------+----------------+
 | Field       | Type         | Null | Key | Default | Extra          |
 +-------------+--------------+------+-----+---------+----------------+
@@ -10,10 +10,10 @@ import lombok.Data;
 | province    | varchar(255) | NO   |     | NULL    |                |
 | city        | varchar(255) | NO   |     | NULL    |                |
 | address2    | varchar(255) | NO   |     | NULL    |                |
-| rest        | varchar(255) | NO   |     | NULL    |                |
 | search      | int          | NO   |     | NULL    |                |
 | views       | int          | NO   |     | 0       |                |
 | rest_phone  | varchar(20)  | YES  |     | NULL    |                |
+| contents    | text         | YES  |     | NULL    |                |
 | user_id     | varchar(45)  | NO   | MUL | NULL    |                |
 | category_id | varchar(255) | YES  | MUL | NULL    |                |
 +-------------+--------------+------+-----+---------+----------------+
@@ -25,11 +25,11 @@ public class Restaurant {
 	private String province;
 	private String city;
 	private String address2;
-	private String rest;
 	private int views;
 	private int search;
 	private int ranking;	
 	private String rest_phone;
+	private String contents;
 	private String img_path;
 	private UserDto user; // UsersDto.user_id : fk 
 	private Category category;
