055d13eda74f099c1cbc35cecc9a7d534ec85fe6
Songtae <onlyyoureyes@naver.com>
2022-10-25 10:36:50 +0900

    merge

---
+++ mainPage/src/main/java/com/project/mainPage/Batchapplication.java
@@ -0,0 +1,10 @@
+/*
+ * @EnableBatchProcessing // 배치 사용을 위한 선언
+ * 
+ * @SpringBootApplication package com.project.mainPage;
+ * 
+ * import org.springframework.boot.SpringApplication;
+ * 
+ * public class Batchapplication { public static void main(String[] args) {
+ * SpringApplication.run(Batchapplication.class, args); } }
+ */
