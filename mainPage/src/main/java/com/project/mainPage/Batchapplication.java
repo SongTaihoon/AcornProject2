7fdbdd4e6826bf0e6f025a6939453c86f9499bd0
Songtae <onlyyoureyes@naver.com>
2022-11-26 19:53:59 +0900

    save

--- mainPage/src/main/java/com/project/mainPage/Batchapplication.java
+++ mainPage/src/main/java/com/project/mainPage/Batchapplication.java
@@ -1,10 +1,10 @@
-/*
- * @EnableBatchProcessing // 배치 사용을 위한 선언
- * 
- * @SpringBootApplication package com.project.mainPage;
- * 
- * import org.springframework.boot.SpringApplication;
- * 
- * public class Batchapplication { public static void main(String[] args) {
- * SpringApplication.run(Batchapplication.class, args); } }
- */
+package com.project.mainPage;
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@EnableBatchProcessing // 배치 사용을 위한 선언
+@SpringBootApplication 
+
+public class Batchapplication { public static void main(String[] args) {
+SpringApplication.run(Batchapplication.class, args); } }
